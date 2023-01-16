c904c3a64a713b5c98b905957d6793dd8f77da50
Ruhama Yared <ruhamayared1@gmail.com>
2023-01-15 20:57:51 -0500

    updated email link

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -9,7 +9,7 @@ function Contact() {
     <section id="contact">
       <h1>Contact</h1>
       <div className="links">
-        <a href="mailto:ruhamayared1@gmail.com"><img className="icon" src={email} alt="email icon" /></a>
+      <a href="mailto:ruhamayared1@gmail.com" target="_blank" rel="noopener noreferrer"><img className="icon" src={email} alt="email icon"/></a>
         <a href="https://www.linkedin.com/in/ruhama-yared/" rel="noopener noreferrer" target="_blank"><img className="icon" src={linkedin} alt="linkedIn logo" /></a>
         <a href="https://github.com/ruhamayared" rel="noopener noreferrer" target="_blank"><img className="icon github" src={github} alt="github logo" /></a>
       </div>
