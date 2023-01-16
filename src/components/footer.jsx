70fd4f2d2f05c891f2516b16dd45b1460f1e5e6e
Ruhama Yared <ruhamayared1@gmail.com>
2023-01-15 20:16:31 -0500

    added link to footer credits

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer>
       <p className="hide">Made with React</p>
-      <p><a href="" rel="noopener noreferrer" target="_blank">Credits</a></p>
+      <p><a href="https://github.com/ruhamayared/portfolio-v1" rel="noopener noreferrer" target="_blank">Credits</a></p>
       <p> &#169; Ruhama Yared 2023</p>
     </footer>
   );
