d24b60f0b7694a3a739987fd0a2d8aa886251057
Ruhama Yared <ruhamayared1@gmail.com>
2023-07-09 21:08:34 -0400

    update bio and resume

--- src/components/bio.jsx
+++ src/components/bio.jsx
@@ -17,7 +17,7 @@ function Bio() {
             I am a software engineer with a strong background in education and public policy. I am particularly interested in exploring the areas of cloud architecture, machine learning, and AI. I am currently a middle and high school math teacher and bring a unique perspective to the field of software engineering with my education background and real-world experience.
           </p>
           <p>
-            <span>Skills: </span>Java, JavaScript/TypeScript, HTML, CSS, Tailwind, React, Node.js, Express, EJS, SQL, PostgreSQL, Mongoose/MongoDB, Python, Django, FastAPI, Flask, Go, Echo, Git, GitHub, Amazon Web Services (AWS), Docker, Kubernetes, Terraform, Ansible, YAML
+            <span>Skills: </span>Java, JavaScript/TypeScript, HTML, CSS, Tailwind, React, Node.js, Express, EJS, SQL, PostgreSQL, Mongoose/MongoDB, Python, Django, FastAPI, Flask, Go, Echo, Git, GitHub, Microsoft Azure, Amazon Web Services (AWS), Docker, Kubernetes, Terraform, Ansible, YAML
           </p>
         </div>
       </div>
